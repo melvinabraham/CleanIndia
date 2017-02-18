63cd2d86ba0b7c2f0094dffb0c5efa46c44e3fef
melvinabraham <melvin.abraham1996@gmail.com>
2017-02-19 00:34:03 +0530

    Linked Login XML to java

--- app/src/main/java/com/mapps/seproject/LoginActivity.java
+++ app/src/main/java/com/mapps/seproject/LoginActivity.java
@@ -2,8 +2,19 @@ package com.mapps.seproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    EditText email;
+    EditText password;
+    Button b;
+    TextView registerText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +22,25 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+        email = (EditText) findViewById(R.id.etLoginEmail);
+        password= (EditText) findViewById(R.id.etLoginPassword);
+        b= (Button) findViewById(R.id.bLogin);
+        registerText= (TextView) findViewById(R.id.textRegister);
+
+
+
+        b.setOnClickListener(this);                             // Onclick
+        registerText.setOnClickListener(this);
+
+
+
+        
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
 
 
