c5357ac02f9573de581ef5e8cd9f5c8bf720e6eb
melvinabraham <melvin.abraham1996@gmail.com>
2017-02-19 00:45:42 +0530

    Login Activity Ready

--- app/src/main/java/com/mapps/seproject/LoginActivity.java
+++ app/src/main/java/com/mapps/seproject/LoginActivity.java
@@ -1,11 +1,21 @@
 package com.mapps.seproject;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity  implements View.OnClickListener{
 
@@ -13,6 +23,8 @@ public class LoginActivity extends AppCompatActivity  implements View.OnClickLis
     EditText password;
     Button b;
     TextView registerText;
+    ProgressDialog progressDialog;
+    FirebaseAuth firebaseAuth;
 
 
 
@@ -26,8 +38,16 @@ public class LoginActivity extends AppCompatActivity  implements View.OnClickLis
         password= (EditText) findViewById(R.id.etLoginPassword);
         b= (Button) findViewById(R.id.bLogin);
         registerText= (TextView) findViewById(R.id.textRegister);
+        progressDialog = new ProgressDialog(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser() != null)   {
+
+            finish();
+            startActivity(new Intent(getApplicationContext(),UserActivity.class));              // If already logged in
 
 
+
+        }
 
         b.setOnClickListener(this);                             // Onclick
         registerText.setOnClickListener(this);
@@ -39,8 +59,85 @@ public class LoginActivity extends AppCompatActivity  implements View.OnClickLis
 
     }
 
+
+    public void LoginUser() {                                               // Check the login credentials
+
+
+        String mail = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+
+        if(TextUtils.isEmpty(mail)) {
+            // email empty
+
+            Toast.makeText(this, "Enter E-Mail", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(pass)) {
+            // password empty
+            Toast.makeText(this, "Enter Password", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        progressDialog.setMessage("Logging IN");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(mail,pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful())    {
+
+                                                                                         // Successful Log In
+                            finish();
+                            startActivity(new Intent(getApplicationContext(),UserActivity.class));
+                            progressDialog.hide();
+
+
+                        }
+
+                        else    {
+
+
+                            progressDialog.hide();                                      // Unsuccessful Log In
+                            Toast.makeText(LoginActivity.this, "Incorrect Details", Toast.LENGTH_SHORT).show();
+                            return;
+
+
+                        }
+
+
+
+
+                    }
+                });
+
+    }
+
+
+
+
+
     @Override
     public void onClick(View v) {
+
+
+        if(v == b)                          // Verify Login Credentials
+        {
+
+            LoginUser();
+
+        }
+
+        if(v == registerText){              // Register New User
+
+            finish();
+            startActivity(new Intent(this,RegisterActivity.class));
+
+        }
+
+
 
 
 
