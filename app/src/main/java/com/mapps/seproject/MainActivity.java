e23282c20a8c1bda170f02bcae9b7c3bc11ed6e1
melvinabraham <melvin.abraham1996@gmail.com>
2017-04-22 00:24:30 +0530

    Merged Changes and Added Database Functionality

--- app/src/main/java/com/mapps/seproject/MainActivity.java
+++ app/src/main/java/com/mapps/seproject/MainActivity.java
@@ -8,7 +8,15 @@ import android.support.annotation.IdRes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
@@ -19,10 +27,75 @@ public class MainActivity extends AppCompatActivity {
     Fragment compose_activity;
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
+    FirebaseAuth firebaseAuth;
+
+    int flag = 0;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+    String userId;
+    String UserEmail = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        final String data = firebaseAuth.getCurrentUser().getEmail();
+
+        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("users");
+
+        /*
+        userId = mDatabase.push().getKey();
+        User user = new User(data);
+        mDatabase.child(userId).setValue(user);
+
+*/
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Log.e("Count" , ""+dataSnapshot.getChildrenCount());
+
+                for(DataSnapshot idSnapshot : dataSnapshot.getChildren())   {
+
+                    //Log.e("Email:",""+idSnapshot.child("email").toString().split("value =")[1].split(" ")[1]);
+                    UserEmail = idSnapshot.child("email").toString().split("value =")[1].split(" ")[1].replaceAll("\\s+","");
+
+                    Log.e("Email",UserEmail);
+                    Log.e("Data",data);
+
+
+                    if(data.equals(UserEmail))
+                    {
+
+                     Log.e("E","Already Exists");
+                     flag =1;
+
+                    }
+
+
+                }
+
+                if(flag == 0)   {
+
+                    userId = mDatabase.push().getKey();
+                    User user = new User(data);
+                    mDatabase.child(userId).setValue(user);
+                    Log.e("E","new");
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed" + databaseError.getMessage());
+
+            }
+        });
+
+
+
 
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
@@ -62,4 +135,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+
+
+
 }
---
+++ app/src/main/java/com/mapps/seproject/User.java
@@ -0,0 +1,26 @@
+package com.mapps.seproject;
+
+/**
+ * Created by melvin on 21/4/17.
+ */
+public class User   {
+
+
+
+    public String email;
+
+
+
+    public User()   {
+
+
+    }
+
+    public User(String email)   {
+
+        this.email = email;
+
+    }
+
+}
+
